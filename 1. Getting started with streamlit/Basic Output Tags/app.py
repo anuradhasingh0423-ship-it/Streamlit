404189ca947bbded9c4d9fd8496d0229d60b3957
Anuradha Singh <anuradhasingh0423@gmail.com>
2026-03-07 21:21:15 +0530

    Added all Streamlit project files

---
+++ 1. Getting started with streamlit/Basic Output Tags/app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+
+
+# Title
+st.title("Getting started with Streamlit")
+
+# Header
+st.header("This is a header")
+
+# Subheader
+st.subheader("This is a subheader")
+
+# Text
+st.text("This is some text")
+
+
+# Markdown
+st.markdown('This is a list of h tags using markdown')   # p
+st.markdown('# GFG')         #h1
+st.markdown('## GFG')        #h2
+st.markdown('### GFG')       #h3
+st.markdown('#### GFG')      #h4
+st.markdown('##### GFG')     #h5
+st.markdown('###### GFG')    #h6
+
+st.markdown('**GFG** is an Ed-Tech')  # Bold
+st.markdown('__GFG__ is an Ed-Tech')  # Bold
+
+st.markdown('*GFG* is an Ed-Tech')  # Italic
+st.markdown('_GFG_ is an Ed-Tech')  # Italic
+
+st.markdown('***GFG*** is an Ed-Tech')  # Bold + Italic
+st.markdown('___GFG___ is an Ed-Tech')  # Bold + Italic
+
+
+st.markdown('- First Point')
+st.markdown('- Second Point')
+
+st.write('range(1,10)')
+st.write(range(1,10))
